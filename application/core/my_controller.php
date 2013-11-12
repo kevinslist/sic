af6bcf86a78bc0a9a048277a83464a054a37301b
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2013-11-12 07:49:57 +0000

    .htaccess core overrides

---
+++ application/core/my_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')){  exit('No direct script access allowed'); }
+
+class my_controller extends kb_controller {
+
+  public function MY_Controller() {
+    parent::__construct();
+  }
+}
---
+++ kb_index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once 'application/third_party/kb/core/kb.php';
+include 'index.php';
