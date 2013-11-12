fae73394c7a60b03cf552030e1bde938e3f2f907
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2013-11-12 07:49:57 +0000

    .htaccess core overrides

---
+++ trunk/application/core/my_loader.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class my_loader extends kb_loader
+{
+    public function __construct()
+    {
+      parent::__construct();
+    }
+}
