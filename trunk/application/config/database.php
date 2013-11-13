344aefeea532c9316779a172f2a0de1c464ce5f7
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2013-11-13 07:15:41 +0000

    link previous version back in

--- trunk/application/config/database.php
+++ trunk/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'sic';
+$db['default']['password'] = 'mp3';
+$db['default']['database'] = 'sic';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
