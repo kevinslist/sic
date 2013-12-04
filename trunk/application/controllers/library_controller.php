ab390e7e62cda6e27f15a909c9501d99d131eb9a
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2013-12-04 07:14:49 +0000

    begin parse folders

--- trunk/application/controllers/library_controller.php
+++ trunk/application/controllers/library_controller.php
@@ -17,56 +17,83 @@ class library_controller extends my_controller {
 
   public function import() {
     $script = dirname(dirname(dirname(__FILE__))) . '/index.php';
-
-    //$full_script = "nohup /usr/bin/php '{$script}' > /dev/null & echo $!";
-    //$full_script = "php {$script} library import_folder"; // > /dev/null & echo $!
-    //$full_script = "/usr/bin/nohup php {$script} library import_folder >> /dev/null";
-    //$full_script = "/usr/bin/nohup /opt/local/bin/php {$script} library import_folder 2>&1 & echo $!";
-    //$full_script = "nohup php {$script} library import_folder 2>&1 & echo $!";
-    //$full_script = "nohup /opt/local/bin/php 2>&1";
-    $full_script = "nice /opt/local/bin/php {$script} library import_folder > /dev/null 2>&1 & echo $!";
-
-    $this->log_import('SCRIPT:' . $full_script);
+    $full_script = "nice /opt/local/bin/php {$script} library import_folder > /opt/local/apache2/logs/sic.log 2>&1 & echo $!";
     $output = shell_exec($full_script);
-    $this->log_import('OUTPUT:' . $output);
     print '1';
   }
 
-  public function import_folder() {
-    $this->log_import('import_folder:import_folder');
-    $this->settings = settings::get();
-    $library_import_status = (!isset($this->settings['library_import_status']) || empty($this->settings['library_import_status'])) ? 0 : (int) $this->settings['library_import_status'];
-
-    switch ($library_import_status) {
-      case 1:
-        $this->settings['library_import_status'] = 1;
-        $this->settings['library_import_id'] = time();
-        settings::save($this->settings);
-        $this->start_folder_scan($this->settings['current_import_path']);
-        break;
-      case 0:
-      default:
-        $this->settings['library_import_status'] = 1;
-        $this->settings['library_import_id'] = time();
-        settings::save($this->settings);
-        $this->start_folder_scan($this->settings['current_import_path']);
-        break;
+  function parse_folders() {
+    $this->log_import('parse_folders: ' . (int)$this->settings['library_import_status']);
+    if(1 == (int)$this->settings['library_import_status']){
+        $dir = 'start-loop';
+        while (!empty($dir)) {
+          kb::db_exec('LOCK TABLE dirs WRITE');
+          $dir = kb::db_value('SELECT name FROM dirs LIMIT 0, 1');
+          if (!empty($dir)) {
+            kb::db_delete('dirs', array('name' => $dir));
+          }
+          kb::db_exec('UNLOCK TABLES');
+          $this->log_import('UNLOCK TABLES: ' . $dir);
+          $info = $this->scan_directory($dir);
+          $dir = NULL;
+          $this->log_import('info: ' . var_export($info, TRUE));
+        }
     }
   }
+  
+  static function scan_directory($directory = false){
 
-  public function start_folder_scan($directory = NULL) {
+    $info = array('songs'=>array(), 'images'=>array());
+    
+    if (file_exists($directory) && is_dir($directory)  && is_readable($directory)) {
+      $directory_list = opendir($directory);
+      while ($file = readdir($directory_list)) {
+        if ($file != '.' && $file != '..') {
+          $path = $directory . '/' . $file;
+          if (is_readable($path) && is_file($path)) {
+              $extension = strtolower(end(explode('.', $file)));
+              
+              if ('' == $extension || in_array($extension, self::$song_filter)) {
+                $info['songs'][$path] = filemtime($path);
+              }elseif(in_array($extension, self::$image_filter)) {   
+                $info['images'][$path] = filemtime($path);
+              }else{    
+                db::exec('INSERT INTO xdev_other_files (path) values(?)', $path);
+              }
+          }
+        }
+      }
+      closedir($directory_list);
+      //time()
+    }
+    return $info;
+  }
+
+  public function import_folder() {
+    $this->settings = settings::get();
+    $library_import_status = (!isset($this->settings['library_import_status']) || empty($this->settings['library_import_status'])) ? 0 : (int) $this->settings['library_import_status'];
+  
+
+    $this->log_import('import_folder:' . $library_import_status);
+    if (empty($library_import_status)) {
+      $this->settings['library_import_status'] = 1;
+      $this->settings['library_import_id'] = time();
+      settings::save($this->settings);
+      $this->traverse_folders($this->settings['current_import_path']);
+    }
+    $this->log_import('call parse_folders()');
+    $this->parse_folders();
+  }
+
+  public function traverse_folders($directory = NULL) {
     if (!empty($directory)) {
-
       $count = -1;
       $mp3_count = 0;
       $max_mod = 0;
-
       $dir = false;
-
       if (substr($directory, -1) == '/') {
         $directory = substr($directory, 0, -1);
       }
-      $this->log_import('start_folder_scan: ' . $directory);
 
       if (!file_exists($directory) || !is_dir($directory)) {
         return FALSE;
@@ -80,7 +107,7 @@ class library_controller extends my_controller {
             $path = $directory . '/' . $file;
             if (is_readable($path)) {
               if (is_dir($path)) {
-                $this->start_folder_scan($path);
+                $this->traverse_folders($path);
               } elseif (is_file($path)) {
                 $extension = strtolower(end(explode('.', $file)));
                 if (empty($extension) || in_array($extension, self::$song_filter)) {
@@ -151,3 +178,14 @@ class library_controller extends my_controller {
   }
 
 }
+
+/*
+ * 
+
+    //$full_script = "nohup /usr/bin/php '{$script}' > /dev/null & echo $!";
+    //$full_script = "php {$script} library import_folder"; // > /dev/null & echo $!
+    //$full_script = "/usr/bin/nohup php {$script} library import_folder >> /dev/null";
+    //$full_script = "/usr/bin/nohup /opt/local/bin/php {$script} library import_folder 2>&1 & echo $!";
+    //$full_script = "nohup php {$script} library import_folder 2>&1 & echo $!";
+    //$full_script = "nohup /opt/local/bin/php 2>&1";
+ */
