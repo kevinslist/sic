fb397457e4d0bb535cb46a69b77e45dc28e4779d
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-03-17 05:53:28 +0000

    add google signin

---
+++ application/views/home/logged_out.php
@@ -0,0 +1,5 @@
+<div class="home-logged-out-wrapper">
+  <div class="hidden kb-google-auth-load" id="kb-auth-type" data-kb-auth-type="login">
+    <?=$google_signin_block?>
+  </div>
+</div>
---
+++ application/views/layouts/sic-music/five-section-layout.php
@@ -0,0 +1,8 @@
+<div id="layout-header" class="ui-layout-north"></div>
+<div id="layout-footer" class="ui-layout-south">Loading...</div>
+<div id="navigation-helper" class="ui-layout-east">
+  <div id="app-logger"></div>
+</div>
+<div id="application-navigation" class="ui-layout-west">Loading...</div>
+<div id="layout-playlist" class="ui-layout-center">Loading...</div>
+<div id="sic-menu-holder"></div>
