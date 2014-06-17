980ffc220f89a2b2de2d4ccf0b70fbde8fb746ae
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-06-17 06:40:53 +0000

    simplify

--- application/controllers/lights/lights_home_controller.php
+++ application/controllers/lights/lights_home_controller.php
@@ -1,7 +1,10 @@
 <?php
 
 class lights_home_controller extends my_controller {
+  public function __construct() {
+    parent::__construct();
+  }
   public function index(){
-    die('<div>settings home</div>');
+    die('<div>KB: ' . kb_hue::get_ip() . '</div>');
   }
 }
--- application/views/global/main-menu.php
+++ application/views/global/main-menu.php
@@ -1,7 +1,7 @@
 <div id="sic-main-item-main-menu" class="sic-main-item-wrapper home-logged-in-wrapper" data-sic-init-function="sic_init_main_menu">
   <ul id="sic-main-menu">
-    <li data-sic-load-view="music">MusicSax</li>
-    <li data-sic-load-view="ligts">Hue Lights</li>
+    <li data-sic-load-view="music">Music</li>
+    <li data-sic-load-view="lights/lights-home">Hue Lights</li>
     <li data-sic-load-view="remote">Smart Hub</li>
     <li data-sic-load-view="settings/settings-home">Sic Settings</li>
   </ul>
