1cce9c5b3aac331448d4b82ef5ada7f213c8db98
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-06-21 09:47:38 +0000

    toggle lights

---
+++ trunk/application/views/lights/lights-main-menu.php
@@ -0,0 +1,5 @@
+<div id="sic-main-item-main-menu" class="sic-main-item-wrapper home-logged-in-wrapper" data-sic-init-function="sic_init_main_menu">
+<ul id="sic-main-menu">
+<li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('lights/lights-home');?>" class="main-menu-item">&#x1f4a1; Hue Lights</a></li>
+</ul>
+</div>
