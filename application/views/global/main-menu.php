75fc89dc59bd4d388847da96e533a42b2a3c00c8
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-06-21 09:47:38 +0000

    toggle lights

--- application/views/global/main-menu.php
+++ application/views/global/main-menu.php
@@ -1,8 +1,8 @@
 <div id="sic-main-item-main-menu" class="sic-main-item-wrapper home-logged-in-wrapper" data-sic-init-function="sic_init_main_menu">
   <ul id="sic-main-menu">
-    <li data-sic-load-view="music">Music</li>
-    <li data-sic-load-view="lights/lights-home">Hue Lights</li>
-    <li data-sic-load-view="remote">Smart Hub</li>
-    <li data-sic-load-view="settings/settings-home">Sic Settings</li>
+      <li data-sic-load-view="music"><a href="music" class="main-menu-item">&#9835; music</a></li>
+    <li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('lights/lights-home');?>" class="main-menu-item">&#x1f4a1; Hue Lights</a></li>
+    <li data-sic-load-view="remote"><a href="remote" class="main-menu-item">Smart Hub</a></li>
+    <li data-sic-load-view="settings/settings-home"><a href="settings/settings-home" class="main-menu-item">Sic Settings</a></li>
   </ul>
 </div>
