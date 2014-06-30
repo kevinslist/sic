0ca204c58632297d540ef63f5ed62994b1e9cb02
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-06-30 07:12:49 +0000

    hue updates

--- application/controllers/lights/lights_home_controller.php
+++ application/controllers/lights/lights_home_controller.php
@@ -1,12 +1,15 @@
 <?php
 
 class lights_home_controller extends my_controller {
+  
   public function __construct() {
     parent::__construct();
+    $this->page_title('lights');
+    hue::init();
   }
   public function index(){
-    $r = hue::do_hueg();
-    //$this->kb_content[] = implode("<br />\r\n", $r);
+    $r = hue::do_hue();
+    $this->kb_content[] = implode("<br />\r\n", $r);
     $this->render_page(kb::view('lights/lights-main-menu'));
   }
 }
