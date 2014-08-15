6bd153f30c2655063715a266fd63431681ce013b
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-08-15 06:56:56 +0000

    router REST interface

--- application/controllers/remote/remote_home_controller.php
+++ application/controllers/remote/remote_home_controller.php
@@ -6,8 +6,13 @@ class remote_home_controller extends my_controller {
     parent::__construct();
     $this->page_title('the matrix');
   }
-  public function index(){
-    gefen_8x8_matrix::init();
-    $this->render_page(kb::view('remote/remote-main-menu'));
+  public function index($command){
+    gefen_8x8_matrix::debug('index controller called:' . $command);
+    //$this->render_page(kb::view('remote/remote-main-menu'));
+  }
+  
+  public function route($input = NULL, $output = NULL){
+    gefen_8x8_matrix::debug('route controller called:' . $input);
+    gefen_8x8_matrix::route($input, $output);
   }
 }
