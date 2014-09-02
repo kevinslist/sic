24935b2f7fbbd4689d5989ff8ed6b5934882da46
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-09-02 17:13:28 +0000

    fix signal read still needs aux fix

--- application/controllers/proc_rf_remote_reader_controller.php
+++ application/controllers/proc_rf_remote_reader_controller.php
@@ -2,11 +2,19 @@
 
 class proc_rf_remote_reader_controller extends my_controller {
 
-  public function index() {
+  public function index($arg) {
+
     $this->load->helper('process_rtl433');
     $d = dirname(dirname(__FILE__));
-    $s = $d . '/third_party/kb/builds/rtl443/build/src/rtl_433 -a -D 2>&1';
+    $remote_codes = array(
+        'a' => '433920000',
+        'b' => '418920000',
+    );
+    $s = $d . '/third_party/kb/builds/rtl443/build/src/rtl_433 -a -D -f';
     process_rtl433::$script_command = $s;
+    process_rtl433::$script_output = '2>&1';
+    process_rtl433::$script_frequency = $remote_codes[$arg];
+    process_rtl433::$script_remote = $arg;
     process_rtl433::start();
     
   }
