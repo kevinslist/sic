109301b36da74fa7569475be49015312ff810ff2
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-09-13 11:00:39 +0000

    fix codes based on min/max add multiple remotes

--- application/controllers/proc_rf_remote_reader_controller.php
+++ application/controllers/proc_rf_remote_reader_controller.php
@@ -2,21 +2,20 @@
 
 class proc_rf_remote_reader_controller extends my_controller {
 
-  public function index($arg) {
-
+  public function index($arg = NULL) {
+    exec("pgrep rtl_433", $output);
+    if(count($output)){
+      foreach($output as $line){
+        $kill = 'kill -9 ' . (int)$line;
+        print $kill . PHP_EOL;
+        exec("$kill");
+        sleep(1);
+      }
+    }
     $this->load->helper('process_rtl433');
-    $d = dirname(dirname(__FILE__));
-    $remote_codes = array(
-        'a' => '433920000',
-        'b' => '418920000',
-    );
-    $s = $d . '/third_party/kb/builds/rtl443/build/src/rtl_433 -a -D -f';
-    process_rtl433::$script_command = $s;
-    process_rtl433::$script_output = '2>&1';
-    process_rtl433::$script_frequency = $remote_codes[$arg];
-    process_rtl433::$script_remote = $arg;
-    process_rtl433::start();
-    
+    $app_directory = dirname(dirname(__FILE__));
+    process_rtl433::start($app_directory, $arg);
+   
   }
 
 }
