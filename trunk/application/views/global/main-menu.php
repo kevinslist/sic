7e45d3104ffcf9f59fab61d33e9eb3f8c69fefe7
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2014-12-28 07:04:11 +0000

    update logging add strobe light effect

--- trunk/application/views/global/main-menu.php
+++ trunk/application/views/global/main-menu.php
@@ -1,6 +1,6 @@
 <div id="sic-main-item-main-menu" class="sic-main-item-wrapper home-logged-in-wrapper" data-sic-init-function="sic_init_main_menu">
   <ul id="sic-main-menu">
-    <li data-sic-load-view="remote/remote-home"><a href="<?php echo site_url('remote/remote-home'); ?>" class="main-menu-item"><span class="fa"></span> Matrix</a></li>
+    <li data-sic-load-view="remote/remote-home"><a href="<?php echo site_url('remote/remote-home'); ?>" class="main-menu-item">&#9084; Matrix</a></li>
     <li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('lights/lights-home'); ?>" class="main-menu-item"><span class="fa fa-lightbulb-o"></span> Hue Lights</a></li>
     <li data-sic-load-view="music"><a href="music" class="main-menu-item"><span class="glyphicon glyphicon-headphones"></span> music</a></li>
     <li data-sic-load-view="settings/settings-home"><a href="settings/settings-home" class="main-menu-item">Sic Settings</a></li>
--- trunk/application/views/lights/lights-main-menu.php
+++ trunk/application/views/lights/lights-main-menu.php
@@ -1,5 +1,8 @@
 <div id="sic-main-item-main-menu" class="sic-main-item-wrapper home-logged-in-wrapper" data-sic-init-function="sic_init_main_menu">
 <ul id="sic-main-menu">
+<li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('/');?>" class="main-menu-item">&#x2388; HOME</a></li>
 <li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('lights/lights-home');?>" class="main-menu-item">&#x1f4a1; Hue Lights</a></li>
+<li data-sic-load-view="lights/lights-home"><a href="<?php echo site_url('lights/lights-home/strobe');?>" class="main-menu-item">!STROBE!</a></li>
 </ul>
+<div class='kb-debug'><pre><?php print_r(hue::$info); ?></pre></div>
 </div>
