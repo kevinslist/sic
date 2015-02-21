16136b84cd6424604906835e60607193b3c8a96c
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2015-02-21 02:26:36 +0000

    use db to process signal queue

--- trunk/application/controllers/remote/queue_controller.php
+++ trunk/application/controllers/remote/queue_controller.php
@@ -19,7 +19,8 @@ class queue_controller extends my_controller {
 
     $current_signal = unserialize(base64_decode(urldecode($base_64_command)));
 
-    if (is_array($current_signal) && !empty($current_signal['remote-id']) && !empty($current_signal['signal-id']) && !empty($current_signal['last-signal'])) {
+    if (is_array($current_signal) && !empty($current_signal['remote_command_remote_id']) 
+                                  && !empty($current_signal['remote_command_signal_id']) ) {
       self::_do_queue_signal($current_signal, $base_64_command);
     }else {
       print '<<< ! QUEUE SIGNAL RECEIVED INVALID >>>' . PHP_EOL;
@@ -28,33 +29,7 @@ class queue_controller extends my_controller {
 
   static function _do_queue_signal($s = null, $base_64_command = null) {
     self::$remote_command_inserted_time = time();
-    $params = array(
-      'remote_command_is_repeat' => $s['is-repeat'],
-      'remote_command_remote_id' => $s['remote-id'],
-      'remote_command_time_sent' => $s['last-signal'],
-      'remote_command_signal_id' => $s['signal-id'],
-      'remote_command_inserted_time' => self::$remote_command_inserted_time,
-    );
-    kb::db_insert('remote_commands', $params);
-    /*
-    try {
-      $key = kb::config('KB_CONFIG_ROUTER_INFO_SEM_LOCK_PORT');
-      self::$semaphore = sem_get($key);
-      $locked = sem_acquire(self::$semaphore);
-      if ($locked) {
-        $signal_queue_key = kb::config('KB_SIGNAL_QUEUE_KEY');
-        $q = kb::mval($signal_queue_key);
-        $q[md5($base_64_command)] = $current_signal;
-        kb::mval($signal_queue_key, $q);
-      }
-    } catch (Exception $ex) {
-      
-    } finally {
-      if (!empty(self::$semaphore)) {
-        sem_release(self::$semaphore);
-      }
-    }
-     * 
-     */
+    $s['remote_command_inserted_time'] = self::$remote_command_inserted_time;
+    kb::db_insert('remote_commands', $s);
   }
 }
