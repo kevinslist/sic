95a8eec0d5d6616fbfa564b7bb7d07afb67e6e69
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2015-02-21 08:50:38 +0000

    updates for init functions and static info arrays

--- trunk/application/controllers/remote/queue_controller.php
+++ trunk/application/controllers/remote/queue_controller.php
@@ -8,10 +8,8 @@ class queue_controller extends my_controller {
 
     if (is_array($signal) && !empty($signal['remote_command_remote_id']) 
                      && !empty($signal['remote_command_signal_id']) ) {
-      if(isset(config_remote::$remote_map[$signal['remote_command_remote_id']])){
         $signal['remote_command_inserted_time'] = time();
         kb::db_insert('remote_commands', $signal);
-      }
     }else {
       //print '<<< ! QUEUE SIGNAL RECEIVED INVALID >>>' . PHP_EOL;
     }
