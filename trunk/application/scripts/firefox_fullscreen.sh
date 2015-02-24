4a7eb2ebcbf7cf46a806998049e4959a28db0cd5
kb@kevinslist.com <kb@kevinslist.com@9668c490-4a5d-ec17-878d-6d57bfc41e2e>
2015-02-24 07:54:12 +0000

    check if firefox fullscreen else f11

--- trunk/application/scripts/firefox_fullscreen.sh
+++ trunk/application/scripts/firefox_fullscreen.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-export DISPLAY=:0.0 # eye
-ps aux | grep firefox-trunk | awk '{print $2}' | xargs kill -9; # kill me
-firefox-trunk -url "https://k/help" # leave me alone
- #sleep 1 # hehehe
- #xdotool key F11 # hit full screen
+export DISPLAY=:0.0
+ps aux | grep firefox-trunk | awk '{print $2}' | xargs kill -9;
+firefox-trunk -url "https://k/help" > /dev/null 2>&1 &
+sleep 1 
+
+WINDOW=$(echo $(xwininfo -id $(xdotool getactivewindow) -stats | \
+                egrep '(Width|Height):' | \
+                awk '{print $NF}') | \
+         sed -e 's/ /x/')
+SCREEN=$(xdpyinfo | grep -m1 dimensions | awk '{print $2}')
+if [ "$WINDOW" != "$SCREEN" ]; then
+    xdotool key F11 # hit full screen
+    echo 'do make fullscreen'
+fi
+
+
+
+
+
+
+
+
+#xdotool key F11 # hit full screen
 
 
 #Tab
